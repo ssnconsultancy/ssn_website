af3cff70d26c59b5f64dae582d0f1c03dae44d01
SSN_Consultancy <ssnconsultancy@outlook.com>
2025-05-19 01:29:38 +0530

    Updates

--- index.js
+++ index.js
@@ -101,6 +101,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 var vite_config_default = defineConfig({
+  base: "/ssn_website/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
