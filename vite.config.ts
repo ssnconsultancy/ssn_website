44bfea73e0a813b2b75f0570f20c48923f40a5f9
SSN_Consultancy <ssnconsultancy@outlook.com>
2025-05-19 01:29:05 +0530

    misc changes 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/ssn_website/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
